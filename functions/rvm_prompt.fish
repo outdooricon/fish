486fa56739b085f45bc4afe38f9e8b5468263efc
Johnathon Sanders <johnathon.sanders@novu.com>
2013-08-20 10:03:08 -0500

    conditional rvm output, only within bundler

--- functions/rvm_prompt.fish
+++ functions/rvm_prompt.fish
@@ -2,10 +2,25 @@ function rvm_prompt
   set -l yellow (set_color -o yellow)
 #  if [ (which rvm) ]
   if type -t rvm >/dev/null
-    set -l identifier (_run-with-bundler rvm tools identifier)
-    echo "$yellow$identifier "
+    if begin; _bundler-installed; and _within-bundled-project; end
+      set -l identifier (command bundle exec rvm tools identifier)
+      echo "$yellow$identifier "
+    end
   else
     echo ""
   end
+end
+
+function _bundler-installed
+  which bundle >/dev/null ^&1
+end
+
+function _within-bundled-project
+  set -l check_dir $PWD
+  while [ $check_dir != "/" ]
+    test -f "$check_dir/Gemfile"; and return
+    set check_dir (dirname $check_dir)
+  end
+  false
 end
 
